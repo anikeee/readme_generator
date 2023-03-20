4b93abfb24d51b0e27bc016d4b45710445681e05
anikeee <muid.hasan@gmail.com>
2023-03-20 04:10:00 +0000

    added question

--- generate-readme.js
+++ generate-readme.js
@@ -1,3 +1,12 @@
 const inquirer = require('inquirer');
 const fs = require('fs');
 
+const questions = [
+    {
+        type: 'input',
+        name: 'title',
+        message: 'What is the title of your project?',
+    },
+    // ... add more questions here
+];
+
